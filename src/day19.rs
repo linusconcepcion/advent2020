430d625236e3fa194f4acd67cf00133f1b17022d
Linus Concepcion <linusconcepcion@outlook.com>
2021-12-25 23:28:11 -0500

    compiles

--- src/day19.rs
+++ src/day19.rs
@@ -9,59 +9,71 @@ pub fn go() {
         .expect("Could not read the input file.");
 
     let lines : Vec<&str> = input.split(util::LINE_ENDING).collect();
-    let mut rules : HashMap<i32, Rule> = lines.into_iter().filter(|&l| {l.contains(":")})
-        .map(|l| {Rule::create_tuple_mut(l)})
+    let newlines = lines.clone();
+
+    let rules : HashMap<i32, Rule> = newlines.into_iter().filter(|&l| {l.contains(":")})
+        .map(|l| {Rule::create_tuple(l)})
         .collect();
 
-    let rulezero = rules.get_mut(&0).unwrap();
-    let possible = rulezero.get_possible_values(&mut rules);
+    let mut cache : HashMap<i32, Vec<String>> = HashMap::new();
+    let possible = Rule::get_possible_values_from_id(&rules, &mut cache, 0);
+
+    let count = possible.len();
+    //let count = lines.into_iter().filter(|&l| {(l.starts_with("a") || l.starts_with("b")) && possible.contains(&l.to_string())}).count();
+    println!("count: {}", count);
 }
 
 struct Rule {
     number: i32,
-    definition: String,
-    possible_values: Vec<String>
+    definition: String
 }
 
 impl Rule {
     fn new(line: &str) -> Self {
         let parts: Vec<&str> = line.split(": ").collect();
-        Rule { number: parts[0].parse::<i32>().unwrap(), definition: parts[1].to_string(), possible_values: vec![]}
+        Rule { number: parts[0].parse::<i32>().unwrap(), definition: parts[1].to_string() }
     }
 
-    fn create_tuple_mut(line: &str) -> (i32, Rule) {
-        let mut rule = Rule::new(line);
+    fn create_tuple(line: &str) -> (i32, Rule) {
+        let rule = Rule::new(line);
         (rule.number, rule)
     }
 
-    fn get_possible_values(&mut self, all: &mut HashMap<i32, Rule>) -> &Vec<String> {
-        if self.possible_values.len()==0 {
-            // process the definition
-            if self.definition.contains("\"") {
-                self.possible_values.push(self.definition[1..2].to_string());
-            } else {
-                let ors = self.definition.split(" | ");
-                self.possible_values = ors.map(|o| { Rule::expand_value(o, all) })
-                    .flatten().collect();
-            }
-        }
-
-        &self.possible_values
+    fn get_possible_values_from_id(all: &HashMap<i32, Rule>, cache: &mut HashMap<i32, Vec<String>>, id: i32) -> Vec<String> {
+        let rulezero = all.get(&id).unwrap();
+        rulezero.get_possible_values(all, cache)
     }
 
-    fn expand_value(ids: &str, all: &mut HashMap<i32, Rule>) -> Vec<String> {
+    fn get_possible_values(&self, all: &HashMap<i32, Rule>, cache: &mut HashMap<i32, Vec<String>>) -> Vec<String> {
+        let result = match cache.get(&self.number) {
+            Some(s) => s,
+            None => {
+                if self.definition.contains("\"") {
+                    return vec![self.definition[1..2].to_string()];
+                } else {
+                    let ors = self.definition.split(" | ");
+                    return ors.map(|o| { Rule::expand_value(o, all, cache) })
+                        .flatten().collect();
+                }
+            }
+        };
+        
+        result.clone()
+    }
+
+    fn expand_value(ids: &str, all: &HashMap<i32, Rule>, cache: &mut HashMap<i32, Vec<String>>) -> Vec<String> {
         let idvals: Vec<i32> = ids.split(' ').map(|s| { s.parse::<i32>().unwrap() }).collect();
         if idvals.len()==1 {
             let x = idvals[0];
-            return all.get_mut(&x).unwrap().get_possible_values(all).to_vec();
+            return Rule::get_possible_values_from_id(all, cache, x).to_vec();   //all.get_mut(&x).unwrap().get_possible_values(all).to_vec();
         }
         else if idvals.len()==2 {
             let x = idvals[0];
             let y = idvals[1];
             let mut result = Vec::<String>::new();
-            for x1 in all.get_mut(&x).unwrap().get_possible_values(all) {
-                for y1 in all.get_mut(&y).unwrap().get_possible_values(all) {
-                    result.push(x1.to_owned() + y1);
+            for x1 in Rule::get_possible_values_from_id(all, cache, x) { //all.get_mut(&x).unwrap().get_possible_values(all) {
+                for y1 in Rule::get_possible_values_from_id(all, cache, y) { //all.get_mut(&y).unwrap().get_possible_values(all) {
+                    result.push(x1.clone() + &y1);
                 }
             }
             return result;
@@ -71,10 +83,10 @@ impl Rule {
             let y = idvals[1];
             let z = idvals[2];
             let mut result = Vec::<String>::new();
-            for x1 in all.get_mut(&x).unwrap().get_possible_values(all) {
-                for y1 in all.get_mut(&y).unwrap().get_possible_values(all) {
-                    for z1 in all.get_mut(&z).unwrap().get_possible_values(all) {
-                        result.push(x1.to_owned() + y1 + z1);
+            for x1 in Rule::get_possible_values_from_id(all, cache, x) {//all.get_mut(&x).unwrap().get_possible_values(all) {
+                for y1 in Rule::get_possible_values_from_id(all, cache, y) {//all.get_mut(&y).unwrap().get_possible_values(all) {
+                    for z1 in Rule::get_possible_values_from_id(all, cache, z) {//all.get_mut(&z).unwrap().get_possible_values(all) {
+                        result.push(x1.clone() + &y1 + &z1);
                     }
                 }
             }
